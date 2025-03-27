4ddf0218ecceb873fb2617a072b0f29aa96c5bc8
shadowvv <57322722@163.com>
2025-03-27 13:39:31 +0800

    修改脚本

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # 变量
-JAR_FILE="/opt/myapp/KungFu_Server-1.0.jar"
+JAR_FILE="/opt/myapp/Deploy/KungFu_Server-1.0.jar"
+LIB_DIR="/opt/myapp/Deploy/lib"
+
 LOG_FILE="/opt/myapp/logs/app.log"
 
 # 确保日志目录存在
@@ -14,14 +16,17 @@ fi
 
 # 停止现有的Java应用程序（如果正在运行）
 if pgrep -f "KungFu_Server-1.0.jar"; then
-  pkill -f "KungFu_Server-1.0.jar"
+  sudo pkill -f "KungFu_Server-1.0.jar"
   echo "已停止现有的Java应用程序。"
 else
   echo "未找到现有的Java应用程序。"
 fi
 
+# 加载lib文件夹下的所有jar包
+CLASSPATH=$(find "$LIB_DIR" -name '*.jar' | tr '\n' ':')
+
 # 启动新的Java应用程序
-nohup java -jar "$JAR_FILE" > "$LOG_FILE" 2>&1 &
+sudo nohup java -cp "$CLASSPATH$JAR_FILE" -jar "$JAR_FILE" > "$LOG_FILE" 2>&1 &
 echo "Java应用程序已启动。日志可以在 $LOG_FILE 中找到。"
 
 # 等待并验证应用程序是否启动
